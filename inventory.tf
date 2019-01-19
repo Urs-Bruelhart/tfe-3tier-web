15da470208a7825176997e64df5de6dd113286c7
joestack <joern@joern-online.de>
2019-01-19 10:26:35 +0100

    inventory1

---
+++ inventory.tf
@@ -0,0 +1,50 @@
+## dynamically generate a `inventory` file for Ansible Configuration Automation 
+
+data "template_file" "ansible_web_hosts" {
+    count      = "${var.web_node_count}"
+    template   = "${file("${path.module}/templates/ansible_hosts.tpl")}"
+    depends_on = ["aws_instance.web_nodes"]
+
+      vars {
+        node_name    = "${lookup(aws_instance.web_nodes.*.tags[count.index], "Name")}"
+        ansible_user = "${var.ssh_user}"
+        extra        = "ansible_host=${element(aws_instance.web_nodes.*.private_ip,count.index)}"
+      }
+
+}
+
+data "template_file" "ansible_groups" {
+    template = "${file("${path.module}/templates/ansible_groups.tpl")}"
+
+      vars {
+        jump_host_ip  = "${aws_instance.jumphost.public_ip}"
+        ssh_user_name = "${var.ssh_user}"
+        web_hosts_def = "${join("",data.template_file.ansible_web_hosts.*.rendered)}"
+      }
+
+}
+
+resource "local_file" "ansible_inventory" {
+    content = "${data.template_file.ansible_groups.rendered}"
+    filename = "${path.module}/inventory"
+
+}
+
+
+resource "null_resource" "provisioner" {
+
+  provisioner "file" {
+    source      = "${path.module}/inventory"
+    destination = "~/inventory"
+  
+    connection {
+      type     = "ssh"
+      host     = "${aws_instance.jumphost.public_ip}"
+      user     = "${var.ssh_user}"
+      #password = "${var.root_password}"
+      private_key = "${var.id_rsa_aws}"
+      insecure = true
+    }
+  }
+
+}
