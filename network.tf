8ffbf51a7df2e9b51f852a54219e8d962ac6c105
joestack <joern@joern-online.de>
2019-02-01 07:34:21 +0100

    added tags on VPC

--- network.tf
+++ network.tf
@@ -39,10 +39,14 @@ resource "aws_vpc" "hashicorp_vpc" {
   cidr_block           = "${var.network_address_space}"
   enable_dns_hostnames = "true"
 
+ 
   tags {
-          Name        = "${var.name}-vpc"
-          Environment = "${var.environment_tag}"
+         Name        = "${var.name}-vpc"
+         Environment = "${var.environment_tag}"
+         TTL         = "${var.ttl}"
+         Owner       = "${var.owner}"
   }
+
 
 }
 
